96d650f113a2c81021b45bbecfdaf57caeb020d6
CedReg <cedric.regenass@hf-ict.info>
2017-09-16 13:15:14 +0200

    fixed view.java, changed to hbox+vbox layout

--- workspace/statistics/src/ch/hfict/view/View.java
+++ workspace/statistics/src/ch/hfict/view/View.java
@@ -13,6 +13,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class View extends Application{
@@ -21,13 +23,13 @@ public class View extends Application{
   
   public View() {
     BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Pleas enter one or more digits: ");
     stat.read(r);
-    System.out.println("Con finished boot up gui");
   }
   
   public void start(Stage stage) throws Exception {
 
-    TextField textfld = new TextField("enter values");
+    TextField textfld = new TextField("Enter more digits");
     textfld.setPrefColumnCount(8);
     TextField resultfld = new TextField();
     resultfld.setPrefColumnCount(8);
@@ -36,29 +38,34 @@ public class View extends Application{
     calcbtn.setOnAction(new EventHandler<ActionEvent>() {
         @Override
 		public void handle(ActionEvent e) {
-		  //if (e.isConsumed()) {
-		    System.out.println("TEST TEXTBLT");
-		    stat.addNumber(textfld.getText());
-		    resultfld.clear();
-		    resultfld.setText(String.valueOf(stat.getAverage()));
-
-		    //textfld.clear();
-		    //e.consume();
-		  //}
+		  //System.out.println("TEST TEXTBLT");
+		  stat.addNumber(textfld.getText());
+		  resultfld.clear();
+		  resultfld.setText(String.valueOf(stat.getAverage()));
 		}
     });
     Button clearbtn = new Button("Clear");
-    calcbtn.setOnAction(new EventHandler<ActionEvent>() {
+    clearbtn.setOnAction(new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent e) {
-          if (e.isConsumed()) {
-            resultfld.clear();
-            textfld.clear();
-            e.consume();
-          }
+          resultfld.clear();
+          textfld.clear();
         }
     });
+
+    // create hbox + vbox  and add nodes to it alike QT-Stack
+    HBox hb1 = new HBox();
+    HBox hb2 = new HBox();
+    VBox vb1 = new VBox();
     
+    hb1.getChildren().add(textfld);
+    hb1.getChildren().add(calcbtn);
+    hb2.getChildren().add(resultfld);
+    hb2.getChildren().add(clearbtn);
+    vb1.getChildren().add(hb1);
+    vb1.getChildren().add(hb2);
+    
+    /* (FLOWPANE)
     // create pane (layout) and set properties
     FlowPane flowRoot = new FlowPane();
     flowRoot.setPadding(new Insets(2));
@@ -68,9 +75,10 @@ public class View extends Application{
     flowRoot.getChildren().add(calcbtn);
     flowRoot.getChildren().add(resultfld);
     flowRoot.getChildren().add(clearbtn);
+    */
     
     //set pane on the scene (->layout on the widget (or mainwindow))
-    stage.setScene(new Scene(flowRoot, 300, 150));
+    stage.setScene(new Scene(vb1, 300, 150));
     stage.setTitle("HFICT STATISTICS");
     stage.show();
     
