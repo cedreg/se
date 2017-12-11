25d8ccaeeb74929b3acb70702f0f4f6b97664792
CedReg <cedric.regenass@hf-ict.info>
2017-12-11 13:42:26 +0100

    Vor Test Commit

---
+++ workspace/springcalc/src/main/java/example/CalcApp.java
@@ -0,0 +1,19 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CalcApp {
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"service.xml");
+		
+		CalculationService calcService = (CalculationService) context
+				.getBean("statisticsService");
+		
+		System.out.println("Result: " + calcService.calculate());
+	}
+}
